24d533bef16ff5dfbb27e8048ea671d4670d69a2
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-05-13 10:31:14 -0500

    Volviendo al commit anterior para descartar errores

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/signup', (req, res) =>{
+    res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) =>{
+
+});
+
 module.exports = router;
